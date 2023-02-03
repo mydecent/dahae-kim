7efe4966080c7f3a668bb08eb81533782ba5ba7d
Dahae <dahae.kim@toss.im>
2023-02-03 09:31:32 +0900

    fix: washing

--- src/pages/work/index.tsx
+++ src/pages/work/index.tsx
@@ -36,8 +36,8 @@ const WorkPage = () => (
           </LinkTitle>
           <Period>2022.06 ~ Present</Period>
           <Description>
-            {`현재는 토스에서 새롭게 진출하는 오프라인 결제 시장에 편리한 운영 경험을 제공하는 POS 소프트웨어를 개발하고 있어요.
-            Electron, React 등 웹기술을 이용하여 POS를 개발 중이며, 올 상반기에 정식 런칭 예정이에요.\n
+            {`현재는 토스에서 새롭게 진출하는 오프라인 결제 시장에 편리한 운영 경험을 제공하는 POS 소프트웨어를 개발하고 있습니다.
+            Electron, React 등 웹기술을 이용하여 POS를 개발 중이며, 올 상반기에 정식 런칭 예정입니다.\n
             Currently developing POS system for better experience in the offline stores, with web technologies such as Electron, React etc.
             `}
           </Description>
@@ -57,9 +57,9 @@ const WorkPage = () => (
           </LinkTitle>
           <Period>2021.02 ~ 2022.05</Period>
           <Description>
-            {`글로벌 농·축·수산물 무역 거래 플랫폼을 운영하는 트릿지의 회사 웹사이트 및 스태프용 백오피스 시스템의 UI/UX 개선하는 작업을 했어요.
+            {`글로벌 농·축·수산물 무역 거래 플랫폼을 운영하는 트릿지의 회사 웹사이트 및 스태프용 백오피스 시스템의 UI/UX 개선하는 작업을 했습니다.
               상품의 생산부터 배송까지 거래의 전 단계를 책임지는 회사의 메인 비즈니스인 '풀필먼트 솔루션'을 담당하여 웹사이트 및 어드민 사이트를 개발하였고,
-              글로벌 스태프들이 사용할 하이브리드 모바일 앱 개발 및 사내 ERP 시스템을 위한 기반을 구축했어요.\n
+              글로벌 스태프들이 사용할 하이브리드 모바일 앱 개발 및 사내 ERP 시스템을 위한 기반을 구축했습니다.\n
               A website & admin system for global trading platform.
               In charge of fulfillment solution part, which is the main business model of the company, in both website and admin system.
               Built hybrid mobile app of admin system and MVP of ERP system.
@@ -74,9 +74,9 @@ const WorkPage = () => (
           <Title>Chanel LeBlanc 2021</Title>
           <Period>2020.10 ~ 2020.12</Period>
           <Description>
-            {`Chanel Japan에서 진행한 온라인 라이브 스트리밍 이벤트를 위한 마이크로 사이트를 제작했어요. 
-            풀스택으로 클라이언트 및 서버 모두 개발하였으며, 예약현황 및 타임라인에 따른 다이내믹한 자동 화면전환, 웹소켓을 통한 라이브 스트리밍 중 실시간 채팅, 각종 그래프 및 CSV export 기능을 포함한 어드민 대시보드를 구현했어요.
-            (이벤트 진행 후 웹사이트는 종료되어, 작업한 내용의 일부는 아래 이미지로 확인 가능해요)\n
+            {`Chanel Japan에서 진행한 온라인 라이브 스트리밍 이벤트를 위한 마이크로 사이트를 제작했습니다. 
+            풀스택으로 클라이언트 및 서버 모두 개발하였으며, 예약현황 및 타임라인에 따른 다이내믹한 자동 화면전환, 웹소켓을 통한 라이브 스트리밍 중 실시간 채팅, 각종 그래프 및 CSV export 기능을 포함한 어드민 대시보드를 구현했습니다.
+            (이벤트 진행 후 웹사이트는 종료되어, 작업한 내용의 일부는 아래 이미지로 확인 가능합니다)\n
             A microsite for live streaming event of Chanel LeBlanc product lines.
             Worked as full-stack, implemented dynamic page transitions depending on the reservation status and the timelines.
             Supported live chat during live streaming with WebSocket and built admin dashboard with charts & CSV export.
@@ -107,8 +107,8 @@ const WorkPage = () => (
           </LinkTitle>
           <Period>2021.04 ~ 2021.06</Period>
           <Description>
-            {`국내 대표 프로바이오틱 브랜드 듀오락의 글로벌 웹사이트를 구축했어요. 
-            Canvas API 등을 사용한 애니메이션을 통해 다이내믹한 UI를 구현하였으며 반응형, 크로스 브라우징, CMS 기능 커스터마이징 등을 서포트했어요.\n
+            {`국내 대표 프로바이오틱 브랜드 듀오락의 글로벌 웹사이트를 구축했습니다. 
+            Canvas API 등을 사용한 애니메이션을 통해 다이내믹한 UI를 구현하였으며 반응형, 크로스 브라우징, CMS 기능 커스터마이징 등을 서포트했습니다.\n
             A brand website of Duolac, the most recognized probiotic brand in Korea, for the global market.
             Implemented dynamic animated UI using Canvas API and other anmation libraries. 
             Supported responsive design & cross-browsing, integrated CMS.`}
@@ -130,10 +130,10 @@ const WorkPage = () => (
           </LinkTitle>
           <Period>2019.12 ~ 2020.01</Period>
           <Description>
-            {`일본에 본사를 둔 패션 및 라이프스타일 중심의 이커머스 사이트 Milleporte의 카트 및 결제 플로우를 리뉴얼하는 작업을 했어요.
-            인터랙티브한 쇼핑카트 및 one-page 체크아웃 페이지를 통해 복잡한 결제 경험을 보다 편하게 개선했어요.
-            크레딧카드 BlueGate 인증 및 PayPal을 통한 결제 플로우를 구현했어요.
-            (현재는 웹사이트 전체가 다시 리뉴얼되어, 기존에 작업했던 내용은 아래 이미지로 확인 가능해요)\n
+            {`일본에 본사를 둔 패션 및 라이프스타일 중심의 이커머스 사이트 Milleporte의 카트 및 결제 플로우 리뉴얼 작업을 했습니다.
+            인터랙티브한 쇼핑카트 및 one-page 체크아웃 페이지를 통해 복잡한 결제 경험을 보다 편하게 개선했습니다.
+            크레딧카드 BlueGate 인증 및 PayPal을 통한 결제 플로우를 구현했습니다.
+            (현재는 웹사이트 전체가 다시 리뉴얼되어, 기존에 작업했던 내용은 아래 이미지로 확인 가능합니다)\n
             An e-commerce website for fashion & lifestyle based in Japan.
             Improved cart & checkout experience by rebuilding static pages to be interactive and replacing the multi-pages checkout process with a single page checkout.
             Handled the payment process with credit card BlueGate authentication & PayPal.`}
@@ -154,8 +154,8 @@ const WorkPage = () => (
           </LinkTitle>
           <Period>2019.09 ~ 2019.10</Period>
           <Description>
-            {`국내 서비스 로봇 대표 기업 유진로봇의 홈페이지를 리뉴얼 했어요.
-            반응형, 크로스 브라우징을 고려하여 작업했으며, 다국어 지원이 가능한 CMS 커스터마이징 및 Google Analytics를 적용했어요.\n
+            {`국내 서비스 로봇 대표 기업 유진로봇의 홈페이지를 리뉴얼 했습니다.
+            반응형, 크로스 브라우징을 고려하여 작업했으며, 다국어 지원이 가능한 CMS 커스터마이징 및 Google Analytics를 적용했습니다.\n
             A corporate website of Yujin Robot, Korean leading company in the Robot industry.
             Delivered responsive, cross-browser compatible and accessibility compliant website.
             Supported multilingual, integrated CMS & Google Analytics.`}
